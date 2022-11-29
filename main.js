d5a50112389d6f3c4ac9ae1b6a88461477aa9c5e
Geslon Schottz de Andrade <geslon10@hotmail.com>
2022-11-28 22:33:12 -0300

    javascript de sons funcionando

---
+++ main.js
@@ -0,0 +1,21 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+}
