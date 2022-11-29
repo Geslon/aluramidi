7588ea9d1d3e09c76d1f560ec6f74ce5c5fd2add
Geslon Schottz de Andrade <geslon10@hotmail.com>
2022-11-28 23:50:32 -0300

    projeto midi concluido

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
-}
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrato ou seletor inválido');
+    }
+}
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`;
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-
-    contador = contador + 1;
+   
 }
